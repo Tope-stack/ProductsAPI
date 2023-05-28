85ef2da9c15da29c160b1f0b5e5c7b5c4f28a093
Tope-stack <yusufftope70@gmail.com>
2023-05-28 15:04:51 +0100

    Initial Commit

---
+++ ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductAPI.Data;
+using ProductAPI.Models;
+
+namespace ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = _context.Products.ToList();
+            return Ok(products);
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public IActionResult GetProduct([FromRoute] int id) 
+        {
+            var product = _context.Products.Find(id);
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(Product product)
+        {
+            var newProduct = new Product()
+            {
+                ProductId = product.ProductId,
+                ProductName = product.ProductName,
+                Description = product.Description,
+                Amount = product.Amount,
+            };
+
+            _context.Products.Add(newProduct);
+            _context.SaveChanges();
+            return Ok(newProduct);
+        }
+
+        [HttpPut]
+        [Route("{id:int}")]
+        public IActionResult EditProduct(int id, Product product) 
+        {
+            var existingProduct = _context.Products.Find(id);
+            if (existingProduct != null)
+            {
+                existingProduct.ProductName = product.ProductName;
+                existingProduct.Description = product.Description;
+                existingProduct.Amount = product.Amount;
+
+                _context.SaveChanges();
+
+                return Ok(existingProduct);
+            }
+
+            return NotFound();
+        }
+
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public IActionResult DeleteProduct([FromRoute] int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product != null)
+            {
+                _context.Remove(product);
+                _context.SaveChanges();
+                return Ok("Sucessfully deleted product");
+            }
+
+            return NotFound();
+        }
+
+    }
+}
---
+++ ProductAPI/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductAPI.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal Amount { get; set; }
+    }
+}
